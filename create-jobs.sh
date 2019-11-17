0d9cdfe82e8cdc7d9b4ef2bd0f5bfad1ac5dc1ad
Jack Pertschuk <jp954@cornell.edu>
2019-11-16 22:43:45 -0500

    .

--- create-jobs.sh
+++ create-jobs.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-END=99
+END=345
 
 # Expand files into a temporary directory
 mkdir ./jobs
-for i in $(seq -f "%04g" 0  $END)
+for i in $(seq -f "%04g" 100  $END)
 do
   cat job-template.yaml | sed "s/\$ITEM/$i/" > ./jobs/job-$i.yaml
 done
